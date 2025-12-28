5a09ba66f5bb1a84f2490bb500a9e456849c1465
Giuseppe Sutera <36262092+peppegti@users.noreply.github.com>
2025-12-28 17:59:54 +0100

    Add initial papers.js file with research papers data

---
+++ content/papers.js
@@ -0,0 +1,26 @@
+// content/papers.js
+
+const papers = [
+  {
+    title: "Design and Experimental Validation of a 3D-Printed Two-Finger Gripper",
+    link: "https://scholar.google.it/citations?view_op=view_citation&hl=it&user=ZBhhWt0AAAAJ&sortby=pubdate&citation_for_view=ZBhhWt0AAAAJ:5nxA0vEk-isC",
+    authors: "M Habibi, G Sutera, DC Guastella, G Muscato",
+    img: "gallery/paper/no_image_paper.png",
+    video: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+  },
+  {
+    title: "Adaptive Navigation of a Smart Walker with Shared Control",
+    link: "https://scholar.google.it/citations?view_op=view_citation&hl=it&user=ZBhhWt0AAAAJ&sortby=pubdate&citation_for_view=ZBhhWt0AAAAJ:0EnyYjriUFMC",
+    authors: "G Sutera, DC Guastella, F Cancelliere, G Muscato",
+    img: "gallery/paper/no_image_paper.png"
+  },
+  {
+    title: "Autonomous Multi-Robot Exploration in Unknown Environments",
+    link: "https://scholar.google.it/citations?view_op=view_citation&hl=it&user=ZBhhWt0AAAAJ&sortby=pubdate&citation_for_view=ZBhhWt0AAAAJ:xyz123",
+    authors: "G Sutera, A Rossi, L Bianchi",
+    img: "gallery/paper/no_image_paper.png",
+    video: "https://www.youtube.com/watch?v=abcd1234"
+  },
+  {
+    title: "3D Printing for Rapid Robot Prototyping",
+    link: "https://scholar.google.it/citations?view_op=vi_
